e0c1db59f78fa973d7e538ceb7cbb5418829d4de
Chase <harrisonca86@gmail.com>
2019-09-20 10:10:46 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Chase Harrison on 9/20/19.
+//  Copyright © 2019 Chase Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
